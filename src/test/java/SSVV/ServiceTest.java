d89567554f31cdf60f47fea6eaf6dc9af0f8b2dc
Stefan-code-eng1 <jsie2941@scs.ubbcluj.ro>
2024-04-14 23:19:39 +0300

    added all black box tests

--- src/test/java/SSVV/ServiceTest.java
+++ src/test/java/SSVV/ServiceTest.java
@@ -1,6 +1,5 @@
 package SSVV;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,7 +23,7 @@ public class ServiceTest {
     private TemaXMLRepository temaRepo;
     private NotaXMLRepository notaRepo;
 
-    @BeforeEach
+
     public void setUp() {
 
         Validator<Student> studentValidator = new StudentValidator();
@@ -38,17 +37,76 @@ public class ServiceTest {
         service = new Service(studentRepo, temaRepo, notaRepo);
     }
 
+//BLACK-BOX TESTING------------------------------------------------------------------------------------------------
     @Test
-    public void testSaveStudent_Success() {
+    public void testSaveStudent_Failure1() {
+        setUp();
+        int result = service.saveStudent("4", "John Doe", 88);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void testSaveStudent_Failure2() {
+        setUp();
+        int result = service.saveStudent("5", "Jane Doe", 109);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void testSaveStudent_Success1() {
+        setUp();
+        int result = service.saveStudent("4", "John Doe", 110);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void testSaveStudent_Success2() {
+        setUp();
+        int result = service.saveStudent("5", "Jane Doe", 111);
+        assertEquals(1, result);
+    }
+
+
+    @Test
+    public void testSaveStudent_Success3() {
         setUp();
         int result = service.saveStudent("4", "John Doe", 225);
         assertEquals(1, result);
     }
 
     @Test
-    public void testSaveStudent_Failure() {
+    public void testSaveStudent_Success4() {
         setUp();
-        int result = service.saveStudent("5", "Jane Doe", 109);
+        int result = service.saveStudent("5", "Jane Doe", 937);
         assertEquals(1, result);
     }
+
+
+    @Test
+    public void testSaveStudent_Success5() {
+        setUp();
+        int result = service.saveStudent("4", "John Doe", 938);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void testSaveStudent_Failure3() {
+        setUp();
+        int result = service.saveStudent("5", "Jane Doe", 939);
+        assertEquals(1, result);
+    }
+
+
+    @Test
+    public void testSaveStudent_Failure4() {
+        setUp();
+        int result = service.saveStudent("4", "John Doe", 999);
+        assertEquals(1, result);
+    }
+
+
+//WHITE-BOX TESTING------------------------------------------------------------------------------------------------
+
+
+
 }
