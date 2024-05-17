e3663c26cd5b486b996099ddc30cf658b3295140
Stefan-code-eng1 <jsie2941@scs.ubbcluj.ro>
2024-05-17 15:48:21 +0300

    done

--- src/test/java/SSVV/ServiceTest.java
+++ src/test/java/SSVV/ServiceTest.java
@@ -4,7 +4,6 @@ import domain.Nota;
 import domain.Pair;
 import domain.Student;
 import domain.Tema;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import repository.NotaXMLRepository;
 import repository.StudentXMLRepository;
@@ -21,7 +20,7 @@ public class ServiceTest {
     private TemaXMLRepository temaRepo;
     private NotaXMLRepository notaRepo;
 
-    @BeforeEach
+
     public void setUp() {
         studentRepo = mock(StudentXMLRepository.class);
         temaRepo = mock(TemaXMLRepository.class);
@@ -31,24 +30,28 @@ public class ServiceTest {
 
     @Test
     public void testSaveStudent_Failure1() {
+        setUp();
         int result = service.saveStudent("4", "John Doe", 88);
         assertEquals(1, result);
     }
 
     @Test
     public void testSaveStudent_Failure2() {
+        setUp();
         int result = service.saveStudent("5", "Jane Doe", 109);
         assertEquals(1, result);
     }
 
     @Test
     public void testSaveStudent_Success1() {
+        setUp();
         int result = service.saveStudent("6", "John Doe", 110);
         assertEquals(1, result);
     }
 
     @Test
     public void testSaveStudent_Success2() {
+        setUp();
         int result = service.saveStudent("7", "Jane Doe", 111);
         assertEquals(1, result);
     }
@@ -56,12 +59,14 @@ public class ServiceTest {
 
     @Test
     public void testSaveStudent_Success3() {
+        setUp();
         int result = service.saveStudent("8", "John Doe", 225);
         assertEquals(1, result);
     }
 
     @Test
     public void testSaveStudent_Success4() {
+        setUp();
         int result = service.saveStudent("9", "Jane Doe", 937);
         assertEquals(1, result);
     }
@@ -69,12 +74,14 @@ public class ServiceTest {
 
     @Test
     public void testSaveStudent_Success5() {
+        setUp();
         int result = service.saveStudent("10", "John Doe", 938);
         assertEquals(1, result);
     }
 
     @Test
     public void testSaveStudent_Failure3() {
+        setUp();
         int result = service.saveStudent("11", "Jane Doe", 939);
         assertEquals(1, result);
     }
@@ -82,6 +89,7 @@ public class ServiceTest {
 
     @Test
     public void testSaveStudent_Failure4() {
+        setUp();
         int result = service.saveStudent("12", "John Doe", 999);
         assertEquals(1, result);
     }
@@ -90,30 +98,35 @@ public class ServiceTest {
     //WHITE-BOX TESTING------------------------------------------------------------------------------------------------
     @Test
     public void testSaveTema_ValidInput() {
+        setUp();
         int result = service.saveTema("4", "descriere", 10, 5);
         assertEquals(1, result);
     }
 
     @Test
     public void testSaveTema_NullId() {
+        setUp();
         int result = service.saveTema(null, "descriere", 10, 5);
         assertEquals(1, result);
     }
 
     @Test
     public void testSaveTema_NullDesc() {
+        setUp();
         int result = service.saveTema("4", null, 10, 5);
         assertEquals(1, result);
     }
 
     @Test
     public void testSaveTema_InvalidDeadline() {
+        setUp();
         int result = service.saveTema("5", "descriere", 15, 10);
         assertEquals(1, result);
     }
 
     @Test
     public void testSaveTema_InvalidStartline() {
+        setUp();
         int result = service.saveTema("5", "descriere", 12, 0);
         assertEquals(1, result);
     }
@@ -121,12 +134,14 @@ public class ServiceTest {
 
     @Test
     public void testSaveTema_InvalidStartDeadline() {
+        setUp();
         int result = service.saveTema("5", "descriere", 5, 10);
         assertEquals(1, result);
     }
 
     @Test
     public void testSaveTema_SuccessfulSave() {
+        setUp();
         TemaXMLRepository temaXmlRepo = mock(TemaXMLRepository.class);
         when(temaXmlRepo.save(any())).thenReturn(new Tema("5", "descriere", 10, 5));
 
@@ -141,6 +156,7 @@ public class ServiceTest {
 
     @Test
     public void testSaveTema_UnsuccessfulSave() {
+        setUp();
         TemaXMLRepository temaXmlRepo = mock(TemaXMLRepository.class);
         when(temaXmlRepo.save(any())).thenReturn(null);
 
@@ -158,6 +174,7 @@ public class ServiceTest {
 
     @Test
     public void testSaveStudent_Success() {
+        setUp();
         when(studentRepo.save(any(Student.class))).thenReturn(null);
 
         int result = service.saveStudent("6", "John Doe", 110);
@@ -168,6 +185,7 @@ public class ServiceTest {
 
     @Test
     public void testSaveTema_Success() {
+        setUp();
         when(temaRepo.save(any(Tema.class))).thenReturn(null);
 
         int result = service.saveTema("1", "Description", 7, 3);
@@ -178,6 +196,7 @@ public class ServiceTest {
 
     @Test
     public void testAllIntegration() {
+        setUp();
         when(studentRepo.save(any(Student.class))).thenReturn(new Student("123", "Alice Wonderland", 935));
         when(temaRepo.save(any(Tema.class))).thenReturn(new Tema("10", "Assignment Description", 7, 1));
 
